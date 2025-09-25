a74c88226b98e6fae4a65e6764814e0124a75a89
Saksham Goel <saksham.goel29@gmail.com>
2025-09-25 13:40:53 -0400

    Set up readme.md and added transpose matrix function

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 @author: Saksham Goel
 @description: Creating a neural network from scratch in python with no external libraries.
 """
+from PIL.Image import radial_gradient
+
 
 def load_neural_network(fileName: str):
     pass
@@ -12,9 +14,11 @@ def save_neural_network(fileName: str):
 def transpose(x: list[list[float]]) -> list[list[float]]:
     # swaps rows and columns
     z: list[list[float]] = []
-    for row in x:
-        for col in row:
-
+    for col in range(len(x[0])):
+        z.append([])
+        for row in range(len(x)):
+            z[col].append(x[row][col])
+    return z
 
 def matrix_add(x: list[list[float]], y: list[list[float]]) -> list[list[float]]:
     if not (len(x) == len(y) and len(x[0]) == len(y[0])) or len(x) == 0 or len(y) == 0:
@@ -40,3 +44,7 @@ class NeuralNetwork(object):
 
     def backward_pass(self, x):
         pass
+
+if __name__ == '__main__':
+    # Mini tests
+    print(transpose([[1, 2]]))
