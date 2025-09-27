00f1fdf8ce70b5b4a17d0326d967b82e2134a085
Saksham Goel <saksham.goel29@gmail.com>
2025-09-26 22:19:36 -0400

    More documentation to clarify functions and parameters. Completed the model file handling alongside String and Dictionary representations of the Neural Network.

--- learn.md
+++ learn.md
@@ -3,3 +3,7 @@ pausing for the moment for autofill to write the code that I double check.
 
 This however is great, as its like rekindling my joy for programming, it wasn't just about the results,
 but it was also about the bugs we made along the way :)
+
+I have never written SO MANY DETAILED COMMENTS, but no AI, so, every... single... word... of documentation is made by me.
+
+But jokes aside, its still a useful skill to practice in case someone wants to actually use my code library
--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class NeuralNetwork(object):
     """
     The Simple Neural Network made by Saksham Goel
     """
-    def __init__(self, input_size: int = None, output_size: int = None, weights: list[list[list[float]]] = None, biases: list[list[float]] = None, activation_type: str = None):
+    def __init__(self, input_size: int = None, output_size: int = None, weights: list[list[list[float]]] = None, biases: list[list[float]] = None, activation_type: str = None) -> None:
         """
         Initializes the feed forward neural network.
         :param input_size: The number of inputs the model has.
@@ -36,33 +36,74 @@ class NeuralNetwork(object):
             self.biases: list[list[float]] = biases
             self.activation_type: str = activation_type
 
-    def forward_pass(self, x):
+    def forward_pass(self, x: list[float]) -> list[float]:
+        """
+        Generates a softmax output based on the input "x"
+        :param x: Inputs
+        :return: Returns the outputs of the model
+        """
         pass
 
-    def backward_pass(self, x):
+    def backward_pass(self, x: list[float], y: list[float]) -> None:
+        """
+        Performs a backward pass on the neural network to minimize loss and accelerate learning.
+        :param x: Inputs
+        :param y: expected outputs
+        :return: Does not return anything.
+        """
         pass
+    
+    def get_dict_structure(self) -> dict:
+        """
+        Get the structure of the Neural Network as a dictionary.
+        :return: Returns the Model's Structure as a dictionary.
+        """
+        structure = {
+            "version": 1,
+            "input_size": self.input_size,
+            "output_size": self.output_size,
+            "weights": self.weights,
+            "biases": self.biases,
+            "activation_type": self.activation_type
+        }
+        return structure
+
+    def __repr__(self) -> str:
+        return str(self.get_dict_structure())
 
 
 # File Loading/Saving Logic for the Neural Network Library
 def load_neural_network(fileName: str) -> NeuralNetwork:
     """
-    Loads the Neural Network file an d
-    :file_conditions: The .json must be formatted as:
-    {"version": 1, "input_size": int, "output_size": int, "weights": list[list[list[float]]], "biases": list[list[float]], "activation_type": str}
+    Loads the Neural Network file from fileName.
+    :precondition: The .json must be formatted as:
+    {
+        "version": 1, "input_size": int,
+        "output_size": int,
+        "weights": list[list[list[float]]],
+        "biases": list[list[float]],
+        "activation_type": str
+    }
     :param fileName: The local file path to the model's structure (.json)
-    :return: returns the loaded neural network
+    :return: Returns the loaded neural network
     """
     with open(fileName, "r") as f:
         # Yea, maybe later
         # for line in f:
             # dict(zip(*iter(line.split(':'))))
         data = json.loads(f.read())
+    return NeuralNetwork(data["input_size"], data["output_size"], data["weights"], data["biases"], data["activation_type"])
 
-    pass
-    return NeuralNetwork()
 
-def save_neural_network(fileName: str):
-    pass
+def save_neural_network(fileName: str, model: NeuralNetwork) -> None:
+    """
+    Saves the Neural Network "model" to json file.
+    :param fileName: The local file path to the model's structure (.json)
+    :param model: The model to be saved.
+    :return: This function does not return anything.
+    """
+    with open(fileName, "w") as f:
+        json.dump(model.get_dict_structure(), f)
 
 
 if __name__ == '__main__':
